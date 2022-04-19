bbd6dac90513465911b392fc20c10f3c696f2fcc
ubuntu <vmware@sc2-10-187-138-243.nimbus.eng.vmware.com>
2022-04-19 06:51:15 +0000

    Show Astronomy Picture command in palette

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,43 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets'; 
+
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_apod is activated!');
+    console.log('ICommandPalette:', palette);
+
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'apod-jupyterlab';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        if (!widget.isAttached) {
+          // attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        //Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // add the command to the palette
+    palette.addItem({ command, category: 'Tutorial'})
+
   }
 };
 
